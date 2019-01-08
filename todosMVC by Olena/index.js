42895e3c334fc01498f16cf0443ef112640b6f84
OlenaDyskant <35570998+olenadyskant@users.noreply.github.com>
2019-01-08 16:49:25 +0100

    Update index.js

--- todosMVC by Olena/index.js
+++ todosMVC by Olena/index.js
@@ -5,48 +5,34 @@ var newInput = document.createElement('input');
 newInput.setAttribute('type', 'text');
 newInput.setAttribute('class', 'some-input');
 newInput.setAttribute('value', document.getElementById('add-new').value);
-document.querySelector('form').appendChild(newInput);
+
+// document.querySelector('form').appendChild(newInput);
 var checkMark =  document.createElement('input');
 checkMark.setAttribute('type', 'checkbox');
 checkMark.setAttribute('class', 'checker')
-document.querySelector('form').insertBefore(checkMark, newInput);
-i++
+//document.querySelector('form').insertBefore(checkMark, newInput);
+
+var listItem = document.createElement('li');
+listItem.setAttribute('class', 'input-items');
+listItem.appendChild(checkMark);
+listItem.appendChild(newInput);
+
+unList.appendChild(listItem);
+
 document.getElementById('add-new').value = document.querySelector('placeholder');
+
+// document.querySelector('ul').appendChild(listItem);
+// document.querySelector('li').insertBefore(checkMark, newInput);
+
+i++;
 } 
+
+
+// var allInputs = document.getElementsByClassName('some-input');
 
 document.getElementById('add-new').addEventListener('change', addInputField);
 
-
-//var eachObjValue = {};
 var unList = document.createElement('ul');
-unList.setAttribute('class', 'full-list');
-document.getElementById('container').appendChild(unList);
-
-var allInputs = document.getElementsByClassName('some-input').value;
-for (var k = 0; k < allInputs.length; k++){
-  var liItem = document.createElement('li');
-  liItem.value = allInputs[k];
-  document.querySelector('ul').appendChild(liItem);
-  
-}
-
-
-/*for (i = 0; i < allInputs.length; i++){
-  //var currentValue = allInputs[i];   ---> do  need to map each obj to its value?
-  //eachObjValue[currentValue.value] = currentValue;    
-  var unList = document.createElement('ul');
-  unList.setAttribute('class', 'todo-list');
-  document.querySelector('form').appendChild(unList);
-  var liItem = document.createElement('li');
-  unList.appendChild(liItem);
-  liItem.innerText = allInputs[i];         
-  }
-*/
-
-
-
-
-
-//document.querySelector('add-new').addEventListener('change', addInputField);
-
-//moreInputs.shift(document.getElementById('#add-new').addEventListener('click', addInputField));
+unList.setAttribute('id', 'input-list');
+unList.setAttribute('style', 'list-style: none');
+document.querySelector('form').appendChild(unList);
